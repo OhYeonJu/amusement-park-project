e8f93b83ad49fdcf004f9b2df03fdeaa97b020fc
jyk2022 <foodgoder@kakao.com>
2022-11-16 10:42:08 +0900

    2022.11.16(김진영)4
    app.py 합침

--- mainpage_app.py
+++ mainpage_app.py
@@ -14,14 +14,6 @@ def home():
 # jwt토큰 알아보기
 
 
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    bucket_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'buckets': bucket_list})
-
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
 
 
 
