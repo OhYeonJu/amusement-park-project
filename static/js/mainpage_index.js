fd10c6f05c58557d1e4215e324bb33cab41bf9ee
jongyeol2 <114922306+jongyeol2@users.noreply.github.com>
2022-11-17 03:13:55 +0900

    백업을 열심히합시다
    개빡치네요 백업합시다

--- static/js/mainpage_index.js
+++ static/js/mainpage_index.js
@@ -1,3 +1,42 @@
+$(document).ready(function () {
+    mainpage_show();
+});
+
+function mainpage_show() {
+    $.ajax({
+        type: 'GET',
+        url: '/mainpagepost',
+        data: {},
+        success: function (response) {
+            let rows = response['MP_postlists']
+            for (let i = 0; i < 4; i++) {
+                let name = rows[i]['name'];
+                let park = rows[i]['park'];
+                let ride = rows[i]['ride'];
+                let write_title = rows[i]['write_title'];
+                let img = rows[i]['img'];
+
+                let temp_html =`<section>
+                                    <div class="review_slot1_img"><img src="${img}"></div>
+                                    <div class="review_slot1_txt">
+                                        <div class="review_location"><span>장소 : ${park}</span></div>
+                                        <div class="review_name">
+                                        <div class="review_name_title">제목 : ${write_title}</div>
+                                        <div class="review_name_like"><span>좋아요 1</span></div>
+                                        </div>
+                                        <div class="review_content"><span>내용 : ${ride}</span>
+                                        </div>
+                                        <div class="review_date"><span>작성자 : ${name}</span></div>
+                                    </div>
+                                </section>`
+
+                $('#mainpage_reviewpost').append(temp_html)
+            }
+        }
+    });
+}
+
+
 // 로그아웃함수 추가
 function logout() {
     $.removeCookie('mytoken');
