a5a6aac8ed74669b1cc19dc41b33b35fc75500ed
오연주 <oyj4569@naver.com>
2022-11-16 22:10:18 +0900

    서브페이지 우측상단 아이디 추가, 로그아웃 버튼 추가_22.11.16_오연주
    서브페이지 우측상단 아이디 추가
    로그아웃 버튼 추가
    필요없는 페이지 삭제

--- static/js/introduction.js
+++ static/js/introduction.js
@@ -4,3 +4,10 @@ for (let i = 0; i < thumbnail.length; i++)
     thumbnail[i].addEventListener("click", function () {
         photo.setAttribute("src", this.getAttribute("src"));
     });
+
+// 로그아웃 함수
+function logout() {
+    $.removeCookie('mytoken');
+    alert('로그아웃!')
+    location.replace("/")
+}
--- static/js/post.js
+++ static/js/post.js
@@ -1,44 +1,50 @@
-
 //post_up 스크립트
 
-        $(document).ready(function () {
-            content_show();
-          });
+$(document).ready(function () {
+    content_show();
+});
 
 
-     function save_content() {
-             let park = $('#park').val()
-            let write_title = $('#write_title').val()
-            let name = $('#name').val()
-            let ride = $('#ride').val()
-            let img = $('#img').val()
-                $.ajax({
-                type: "POST",
-                url: "/post_up",
-                data: {'park_give':park, 'write_title_give':write_title, 'name_give':name, 'ride_give':ride, 'img_give': img},
-                success: function (response) {
-                    alert(response["msg"])
-                    window.location.replace("/post")
-                }})
-        };
+function save_content() {
+    let park = $('#park').val()
+    let write_title = $('#write_title').val()
+    let name = $('#name').val()
+    let ride = $('#ride').val()
+    let img = $('#img').val()
+    $.ajax({
+        type: "POST",
+        url: "/post_up",
+        data: {
+            'park_give': park,
+            'write_title_give': write_title,
+            'name_give': name,
+            'ride_give': ride,
+            'img_give': img
+        },
+        success: function (response) {
+            alert(response["msg"])
+            window.location.replace("/post")
+        }
+    })
+};
 
-        function content_show() {
-            $.ajax({
-                type: 'GET',
-                url: '/post/post_show',
-                data: {},
-                success: function (response) {
-                    let rows = response['postUpLists']
-                    for (let i = 0; i < rows.length; i++) {
-                        let park = rows[i]['park'];
-                        let write_title = rows[i]['write_title'];
-                        let name = rows[i]['name'];
-                        let ride = rows[i]['ride'];
-                        let num = rows[i]['num'];
-                        let img = rows[i]['img'];
+function content_show() {
+    $.ajax({
+        type: 'GET',
+        url: '/post/post_show',
+        data: {},
+        success: function (response) {
+            let rows = response['postUpLists']
+            for (let i = 0; i < rows.length; i++) {
+                let park = rows[i]['park'];
+                let write_title = rows[i]['write_title'];
+                let name = rows[i]['name'];
+                let ride = rows[i]['ride'];
+                let num = rows[i]['num'];
+                let img = rows[i]['img'];
 
 
-                        let temp_html = `<div class="review_post" >
+                let temp_html = `<div class="review_post" >
                                                <div class="img"><img src="${img}" alt="이미지"></div>
                                                 <div class="content">
                                                     <p class="like"><span>놀이공원명: ${park}</span></p><p class="like"><span>좋아요:0</span></p>
@@ -49,24 +55,31 @@
                                                   <button type="button" class="btn btn-outline-success" onclick="delet(${num})">삭제</button></span>
                                                 </div>
                                             </div>`
-                        $('#review_show').append(temp_html)
-                    }
-                }
-            });
+                $('#review_show').append(temp_html)
+            }
+        }
+    });
 
-        };
+};
 
-                        function delet(num) {
-                            $.ajax({
-                                type: "POST",
-                                url: "/post/delete",
-                                data: {num_give: num},
-                                success: function (response) {
-                                    alert(response["msg"])
-                                    window.location.reload()
-                                }
-                            })
-                        };
+function delet(num) {
+    $.ajax({
+        type: "POST",
+        url: "/post/delete",
+        data: {num_give: num},
+        success: function (response) {
+            alert(response["msg"])
+            window.location.reload()
+        }
+    })
+};
+
+//로그아웃함수 추가
+function logout() {
+    $.removeCookie('mytoken');
+    alert('로그아웃!')
+    location.replace("/")
+}
 
 
 
