b88583a1502276f3da34d9791af6dc635b399c7f
jyk2022 <foodgoder@kakao.com>
2022-11-17 10:07:38 +0900

    2022.11.17(김진영) 업데이트
    페이지별 링크 연결

---
+++ static/js/intro.js
@@ -0,0 +1,107 @@
+function login() {
+    $.ajax({
+        type: "POST",
+        url: "/api/login",
+        data: {id_give: $('#userid').val(), pw_give: $('#userpw').val()},
+        success: function (response) {
+            if (response['result'] == 'success') {
+                // 로그인이 정상적으로 되면, 토큰을 받아옵니다.
+                // 이 토큰을 mytoken이라는 키 값으로 쿠키에 저장합니다.
+                $.cookie('mytoken', response['token']);
+
+                alert('로그인 완료!')
+                location.replace("/amusementpark")
+            } else {
+                // 로그인이 안되면 에러메시지를 띄웁니다.
+                alert(response['msg'])
+            }
+        }
+    })
+}
+
+// DB에서 같은 id가 있는지 확인하는 변수
+var id_check = 0;
+
+// 회원가입 함수
+function register() {
+    let signup_userpw = $('#signup_userpw').val()
+    let signup_userpw_check = $('#signup_userpw_check').val()
+    if (id_check == 0) {
+        alert('ID CHECK를 해주세요.')
+    } else if (signup_userpw != signup_userpw_check) {
+        alert('pw가 다릅니다.')
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/api/register",
+            data: {
+                id_give: $('#signup_userid').val(),
+                name_give: $('#signup_username').val(),
+                pw_give: $('#signup_userpw').val()
+            },
+            success: function (response) {
+                if (response['result'] == 'success') {
+                    alert('회원가입이 완료되었습니다.')
+                    location.replace("/")
+                } else {
+                    alert(response['msg'])
+                }
+            }
+        })
+    }
+}
+
+
+function idCheck() {
+    let signup_userid_check = $('#signup_userid').val()
+    console.log(signup_userid_check)
+
+    $.ajax({
+        type: "GET",
+        url: "/api/register/id_check",
+        data: {
+            signup_userid_give: signup_userid_check
+        },
+        success: function (response) {
+            if (response['status'] == 'success') {
+                alert(response['msg'])
+                id_check = 1;
+            } else {
+                alert(response['msg'])
+                id_check = 0;
+            }
+        }
+    })
+}
+
+
+$(function () {
+    $("#login_btn").click(function () {
+        $(".login_modal").modal("show");
+        $(".login_modal").css("padding-right", 0);
+        $("body").css("padding-right", 0)
+    });
+
+    $("#login_modal_close").click(function () {
+        $(".login_modal").modal("hide");
+    });
+
+    $("#login_close_btn").click(function () {
+        $(".login_modal").modal("hide");
+    });
+
+    $("#sign_up_btn").click(function () {
+        $(".sign_up_modal").modal("show");
+        $(".sign_up_modal").css("padding-right", 0);
+        $("body").css("padding-right", 0)
+    });
+
+    $("#sign_up_modal_close").click(function () {
+        $(".sign_up_modal").modal("hide");
+    });
+
+    $("#sign_up_close_btn").click(function () {
+        $(".sign_up_modal").modal("hide");
+    });
+
+});
--- static/js/post.js
+++ static/js/post.js
@@ -1,93 +1,86 @@
-
 //post_up 스크립트
 
-        $(document).ready(function () {
-            content_show();
-          });
+$(document).ready(function () {
+    content_show();
+});
 
 
-     function save_content() {
-            let park = $('#park').val()
-            let write_title = $('#write_title').val()
-            let name = $('#name').val()
-            let ride = $('#ride').val()
-            let content = $('#content').val()
-            let img = $('#img').val()
-          if (park === "놀이공원을 선택해주세요." || write_title == "" || name == "" || ride == "" || img == "" || content == "") {
-                 alert("빈칸을 채워주세요")
-            }else {
-                $.ajax({
-                type: "POST",
-                url: "/post_up",
-                data: {'park_give':park, 'write_title_give':write_title, 'name_give':name, 'content_give': content, 'ride_give':ride, 'img_give': img},
-                success: function (response) {
-                    alert(response["msg"])
-                    window.location.replace("/post")
-                }})
-        }};
+function save_content() {
+    let park = $('#park').val()
+    let write_title = $('#write_title').val()
+    let name = $('#name').val()
+    let ride = $('#ride').val()
+    let img = $('#img').val()
+    $.ajax({
+        type: "POST",
+        url: "/post_up",
+        data: {
+            'park_give': park,
+            'write_title_give': write_title,
+            'name_give': name,
+            'ride_give': ride,
+            'img_give': img
+        },
+        success: function (response) {
+            alert(response["msg"])
+            window.location.replace("/post")
+        }
+    })
+};
 
-        function content_show() {
-            $.ajax({
-                type: 'GET',
-                url: '/post/post_show',
-                data: {},
-                success: function (response) {
-                    let rows = response['postUpLists']
-                    for (let i = 0; i < rows.length; i++) {
-                        let park = rows[i]['park'];
-                        let write_title = rows[i]['write_title'];
-                        let name = rows[i]['name'];
-                        let ride = rows[i]['ride'];
-                        let num = rows[i]['num'];
-                        let content = rows[i]['content'];
-                        let img = rows[i]['img'];
-                        let like = rows[i]['like'];
+function content_show() {
+    $.ajax({
+        type: 'GET',
+        url: '/post/post_show',
+        data: {},
+        success: function (response) {
+            let rows = response['postUpLists']
+            for (let i = 0; i < rows.length; i++) {
+                let park = rows[i]['park'];
+                let write_title = rows[i]['write_title'];
+                let name = rows[i]['name'];
+                let ride = rows[i]['ride'];
+                let num = rows[i]['num'];
+                let img = rows[i]['img'];
 
 
-                        let temp_html = `<div class="review_post" >
+                let temp_html = `<div class="review_post" >
                                                <div class="img"><img src="${img}" alt="이미지"></div>
                                                 <div class="content">
-                                                    <p class="like"><span>놀이공원명: ${park}</span></p><p class="like"><span>좋아요: ${like}</span></p>
-                                                    <p><span>놀이기구:</span> ${ride}</p>
+                                                    <p class="like"><span>놀이공원명: ${park}</span></p><p class="like"><span>좋아요:0</span></p>
                                                     <p><span>제목:</span> ${write_title}</p>
-                                                    <p><span>내용:</span> ${content}</p>
+                                                    <p><span>내용:</span> ${ride}</p>
                                                     <p><span>작성자:</span> ${name}</p>
-                                                  <span class="button"><button type="button" class="btn btn-outline-danger" onclick="postLike(${num}) ">좋아요</button>
+                                                  <span class="button"><button type="button" class="btn btn-outline-danger" onclick=" ">좋아요</button>
                                                   <button type="button" class="btn btn-outline-success" onclick="delet(${num})">삭제</button></span>
                                                 </div>
                                             </div>`
-                        $('#review_show').append(temp_html)
-                    }
-                }
-            });
+                $('#review_show').append(temp_html)
+            }
+        }
+    });
 
-        };
+};
 
-        function delet(num) {
-            $.ajax({
-                type: "POST",
-                url: "/post/delete",
-                data: {num_give: num},
-                success: function (response) {
-                    alert(response["msg"])
-                    window.location.reload()
-                }
-            })
-        };
+function delet(num) {
+    $.ajax({
+        type: "POST",
+        url: "/post/delete",
+        data: {num_give: num},
+        success: function (response) {
+            alert(response["msg"])
+            window.location.reload()
+        }
+    })
+};
 
-        //실패한 좋아요 구현 실패의 흔적들
-              // function postLike(num) {
-                //     console.log(num)
-             //     $.ajax({
-             //         type: 'post',
-             //         url: '/post/post_show',
-             //         data: {num_give : num},
-             //             success: function (response) {
-                //                 alert(response['msg']);
-                //                 window.location.reload()
-                //             }
-                //         });
-                //     }
+//로그아웃함수 추가
+function logout() {
+    $.removeCookie('mytoken');
+    alert('로그아웃!')
+    location.replace("/")
+}
+
 
 
 
