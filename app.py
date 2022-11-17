adcb6de8051dcd0e92ed6bdb3e3a2d2a28a98878
jongyeol2 <114922306+jongyeol2@users.noreply.github.com>
2022-11-17 17:04:29 +0900

    찐찐막
    굳

--- app.py
+++ app.py
@@ -191,5 +191,43 @@ def mainpage():
     MP_postlist = list(db.post_up.find({}, {'_id': False}))
     return jsonify({'MP_postlists': MP_postlist})
 
+
+
+
+
+
+
+@app.route("/introduction", methods=["POST"])
+def introduction_post():
+    name_receive = request.form["name_give"]
+    comment_receive = request.form["comment_give"]
+
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+
+    db.introduction.insert_one(doc)
+    return jsonify({'msg':'댓글 작성 완료!'})
+
+@app.route("/introduction_read", methods=["GET"])
+def introduction_get():
+    comment_list = list(db.introduction.find({},{'_id':False}))
+    return jsonify({'comments':comment_list})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/js/introduction.js
+++ static/js/introduction.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+    set_temp()
+    show_comment()
+});
+
 const photo = document.getElementById("photo");
 const thumbnail = document.querySelectorAll("#gallery > li > img");
 for (let i = 0; i < thumbnail.length; i++){
@@ -12,4 +17,56 @@ function logout() {
     alert('로그아웃!')
     location.replace("/")
 }
+function set_temp(){
+    $.ajax({
+        type: "GET",
+        url: "http://spartacodingclub.shop/sparta_api/weather/seoul",
+        data: {},
+        success: function (response) {
+            $('#temp').text(response['temp'])
+        }
+    })
+}
+function save_comment() {
+    let name = $('#name').val()
+    let comment = $('#comment').val()
+
+    $.ajax({
+        type: "POST",
+        url: "/introduction",
+        data: {'name_give':name, 'comment_give':comment},
+        success: function (response) {
+            alert(response["msg"])
+            console.log(response)
+            window.location.reload()
+        }
+    });
+}
+function show_comment() {
+    $('#comment-list').empty()
+    $.ajax({
+        type: "GET",
+        url: "/introduction_read",
+        data: {},
+        success: function (response) {
+            console.log(response)
+            let rows = response['comments']
+            for (let i = 0; i < rows.length; i++) {
+                let name = rows[i]['name'];
+                let comment = rows[i]['comment'];
+
+                let temp_html = `<div class="card">
+                                    <div class="card-body">
+                                        <blockquote class="blockquote mb-0">
+                                            <p>${comment}</p>
+                                            <footer class="blockquote-footer">${name}</footer>
+                                        </blockquote>
+                                    </div>
+                                </div>`
+                $('#comment-list').append(temp_html)
+            }
+        }
+    });
+}
+    // </script>
 
